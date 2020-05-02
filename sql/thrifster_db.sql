b42b284b70e2b5cb6ebb0d617dcdaf0a2a2b2e43
EC2 Default User <ec2-user@ip-172-31-47-46.us-east-2.compute.internal>
2020-05-02 22:26:30 +0000

    fixed sql

--- sql/thrifster_db.sql
+++ sql/thrifster_db.sql
@@ -16,18 +16,15 @@ CREATE TABLE USERS (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE USERS
-  ADD PRIMARY KEY(ID);
-
-ALTER TABLE USERS
   MODIFY ID INT NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 DROP TABLE IF EXISTS POSTS;
 CREATE TABLE POSTS (
-  ID INT NOT NULL,
+  username varchar(50) COLLATE utf8_unicode_ci NOT NULL, 
+  orderID INT NOT NULL,
   purchaseDate DATE,                                                                                                             
-  customerID INT NOT NULL references USERS(ID),
   itemName varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   description varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   cost DOUBLE NOT NULL,
-  PRIMARY KEY(ID)
+  data mediumblob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
